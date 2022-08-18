e2618ad23159be4180c13058fc699133d7843626
DeepaliN08 <chaitali.nagrale@gmail.com>
2022-08-18 22:33:55 +0530

    Hello.java modified

--- src/test/java/snippet/Hello.java
+++ src/test/java/snippet/Hello.java
@@ -2,8 +2,10 @@ package snippet;
 
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("This is Selenium Program........");
-		
+		System.out.println("********************************");
+		System.out.println("This is Hello World Program........");
+		System.out.println("********************************");
+			
 	}
 }
 
