1e7bbb6298e939f05b43f6224ff75f24d57c8347
DeepaliN08 <111518678+DeepaliN08@users.noreply.github.com>
2022-08-18 22:59:16 +0530

    Update and rename Hello.java to Add.java
    Hello.java changed to Add.java

---
+++ src/test/java/snippet/Add.java
@@ -0,0 +1,12 @@
+package snippet;
+
+public class Hello {
+	public static void main(String[] args) {
+		int a=10, b=20;
+		int c=a+b;
+		System.out.println("Addition : "+c);
+		System.out.println("********************************");
+		
+	}
+}
+
