fe85bb86b82099d3b4c128b58cdfc627fcf85ce1
DeepaliN08 <111518678+DeepaliN08@users.noreply.github.com>
2022-08-18 22:41:59 +0530

    Update Multiply.java
    Logic updated

--- src/test/java/snippet/Multiply.java
+++ src/test/java/snippet/Multiply.java
@@ -2,7 +2,9 @@ package snippet;
 
 public class Multiply {
 	public static void main(String[] args) {
-		System.out.println("This is Multiply Program.....");
+		int a=2, b=5;
+		int c = a*b;
+		System.out.println("Multiplication Result : " +c);
 		
 	}
 }
