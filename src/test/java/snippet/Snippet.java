ab44da8cb044c7b4043450136873a067a8e5aa57
DeepaliN08 <111518678+DeepaliN08@users.noreply.github.com>
2022-08-18 22:38:48 +0530

    Update Snippet.java
    updated

--- src/test/java/snippet/Snippet.java
+++ src/test/java/snippet/Snippet.java
@@ -3,6 +3,10 @@ package snippet;
 public class Snippet {
 	public static void main(String[] args) {
 		System.out.println("Hello World......");
+		System.out.println("Hello World......");
+		System.out.println("Hello World......");
+		System.out.println("Hello World......");
+		System.out.println("Hello World......");
 		
 	}
 }
